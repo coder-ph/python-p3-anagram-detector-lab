5b66b3802de574d0f68b1e0eeb41b84d5135d0fe
Phelix <phelixmbani@gmail.com>
2024-11-29 13:15:48 +0300

    Anagram lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,26 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        
+    def match(self, mylist):
+        sorted_list = []
+        for myword in mylist:
+            if sorted(myword) == sorted(self.word):
+                sorted_list.append(myword)
+        return sorted_list
+            
+           
+        
+        
+        
+    @property
+    def word(self):
+        return self._word   
+
+    @word.setter
+    def word(self, word):
+        self._word = (word )
+        
+
+        
